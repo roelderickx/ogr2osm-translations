f97edcf61c1ea6bb90212a8db0bcdafbe5814a43
roelderickx <roel.derickx@gmail.com>
2021-05-13 18:27:03 +0200

    Convert municipalities to OO ogr2osm

--- databc_ta_municip.py
+++ databc_ta_municip.py
@@ -1,16 +1,24 @@
 '''
 A translation function for DataBC TA_MUNICIP city data. 
-
 '''
 
-    
-def filterTags(attrs):
-    if not attrs:
-        return
-    if 'CODE' in attrs and attrs['CODE'] == 'MU':
-        if 'MUN_NAME' in attrs:
-            return { 'boundary':'administrative', 'admin_level':'8', 'source':'DataBC TA_MUNICIP', 'name':attrs['MUN_NAME'].title() }
-        else:
-            return { 'boundary':'administrative', 'admin_level':'8', 'source':'DataBC TA_MUNICIP' }
-    
-    return tags
+import ogr2osm
+
+class TaMunicipTranslation(ogr2osm.TranslationBase):    
+    def filter_tags(self, attrs):
+        if not attrs:
+            return
+
+        tags = {}
+        if 'CODE' in attrs and attrs['CODE'] == 'MU':
+            if 'MUN_NAME' in attrs:
+                tags['boundary'] = 'administrative'
+                tags['admin_level'] = '8'
+                tags['source'] = 'DataBC TA_MUNICIP'
+                tags['name'] = attrs['MUN_NAME'].title()
+            else:
+                tags['boundary'] = 'administrative'
+                tags['admin_level'] = '8'
+                tags['source'] = 'DataBC TA_MUNICIP'
+
+        return tags
